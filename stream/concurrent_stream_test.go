a4993ef4d30b52f50ad2c8bcea8bf8c695d938c7
Carter <liukefeng2008@hotmail.com>
2022-10-06 15:14:32 +0800

    add testcase for Filter

--- stream/concurrent_stream_test.go
+++ stream/concurrent_stream_test.go
@@ -110,3 +110,57 @@ func TestConcurrentStream_FlatMap(t *testing.T) {
 		})
 	}
 }
+
+func TestConcurrentStream_Filter(t *testing.T) {
+	expectItemsFunc := func(source chan<- any) {
+		for i := 0; i < 1000; i++ {
+			if i%2 == 0 {
+				source <- int64(i)
+			}
+		}
+	}
+	tests := []struct {
+		name        string
+		stream      *concurrentStream
+		expectItems []any
+		ordered     bool
+	}{
+		{
+			name:        "empty stream with no parallelism",
+			stream:      Just([]any{}, WithSync()).(*concurrentStream),
+			expectItems: []any{},
+			ordered:     true,
+		},
+		{
+			name:        "empty stream with parallelism",
+			stream:      Just([]any{}, WithParallelism(4)).(*concurrentStream),
+			expectItems: []any{},
+			ordered:     true,
+		},
+		{
+			name:        "non-empty stream with no parallelism",
+			stream:      Range(0, 1000, WithSync()).(*concurrentStream),
+			expectItems: From(expectItemsFunc, WithSync()).ToIfaceSlice(),
+			ordered:     true,
+		},
+		{
+			name:        "non-empty stream with parallelism",
+			stream:      Range(0, 1000, WithParallelism(4)).(*concurrentStream),
+			expectItems: From(expectItemsFunc, WithSync()).ToIfaceSlice(),
+			ordered:     false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actualItems := test.stream.Filter(func(item any) bool {
+				return item.(int64)%2 == 0
+			}).ToIfaceSlice()
+			if !test.ordered {
+				sort.Slice(actualItems, func(i, j int) bool {
+					return actualItems[i].(int64) < actualItems[j].(int64)
+				})
+			}
+			require.ElementsMatch(t, test.expectItems, actualItems)
+		})
+	}
+}
